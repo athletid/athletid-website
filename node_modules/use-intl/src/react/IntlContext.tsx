5e540bf4ca86bb4481a13d22954a1f74a06cfe46
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-08-28 13:56:14 +0200

    + gitignore

--- node_modules/use-intl/src/react/IntlContext.tsx
+++
@@ -1,6 +0,0 @@
-import {createContext} from 'react';
-import {InitializedIntlConfig} from '../core/IntlConfig';
-
-const IntlContext = createContext<InitializedIntlConfig | undefined>(undefined);
-
-export default IntlContext;
